b89b049f4f0bfeb55126916b81a8c10eb5bd0b4d
Stefan Georgiev <stefan.georgiev@sky.uk>
2019-03-01 10:07:14 +0000

    empty project

---
+++ ContactUsService/ContactUsService.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ContactUsService.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ContactUsService/ContactUsService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ContactUsService.WebApiApplication" Language="C#" %>
