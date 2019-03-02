54fe23a3332fcdb544f74a9a0f68665d278f489c
Stefan Georgiev <stefan.georgiev@sky.uk>
2019-03-02 14:59:06 +0000

    json settins

--- ContactUsService/ContactUsService/App_Start/WebApiConfig.cs
+++ ContactUsService/ContactUsService/App_Start/WebApiConfig.cs
@@ -19,6 +19,12 @@ namespace ContactUsService
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(
+                new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting =
+                Newtonsoft.Json.Formatting.Indented;
         }
     }
 }
