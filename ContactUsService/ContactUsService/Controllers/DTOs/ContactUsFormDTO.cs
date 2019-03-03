39b9d6f23750f4d9f5cfe5a27ebab1bf9e8685f6
Stefan Georgiev <stefan.georgiev@sky.uk>
2019-03-03 21:03:15 +0000

    enforcing model validation on API

--- ContactUsService/ContactUsService/Controllers/DTOs/ContactUsFormDTO.cs
+++ ContactUsService/ContactUsService/Controllers/DTOs/ContactUsFormDTO.cs
@@ -1,10 +1,16 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace ContactUsService.Controllers.DTOs
 {
     public class ContactUsFormDTO
     {
+        [Required]
+        [MaxLength(100)]
         public string fullName { get; set; }
+        [Required]
         public string email { get; set; }
+        [MaxLength(2500)]
         public string message { get; set; }
     }
 }
